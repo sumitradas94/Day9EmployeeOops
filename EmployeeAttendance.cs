5e436b73f39739fbdbeafde5dd7df2d08a28b4cd
sumitradas94 <120413479+sumitradas94@users.noreply.github.com>
2023-01-04 11:54:31 +0530

    Modified for Uc1

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -10,20 +10,21 @@ namespace EmployeeWageOops
 
 
     {
-        public static void CheckAttendance()
+       public static int CheckAttendance(int Employeee)
         {
-            int IS_FULL_TIME = 1;
             Random random = new Random();
-            int empCheck = random.Next(2);
-
-            if (empCheck == IS_FULL_TIME)
+            int check = random.Next(2);
+            if (Employeee == check)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("\n employee is present");
+
+
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("\n employee is absent");
             }
+            return check;
         }
     }
 }
