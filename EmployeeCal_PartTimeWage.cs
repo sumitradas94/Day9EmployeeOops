51bc13e37d5eff217400c4121749df0c72b1cb61
sumitradas94 <sumitradas94@gmail.com>
2023-01-04 12:16:53 +0530

    Employee parttime wage

---
+++ EmployeeCal_PartTimeWage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageOops
+{
+    public class EmployeeCal_PartTimeWage
+    {
+        public void Cal_Pwage()
+        {
+            Console.WriteLine("Program for Calculating Employee Wage !!");
+
+            int wagePerHour = 20;
+            int workingHours = 8;
+            int Totalwork = 0;
+            int Employeee = 1;
+            int atten = EmployeeAttendance.CheckAttendance(Employeee);
+
+            if (atten == 1)
+            {
+
+                Totalwork = wagePerHour * workingHours;
+                Console.WriteLine("total work : " + Totalwork);
+            }
+            else if (atten == 2)
+            {
+                Totalwork = wagePerHour * (workingHours / 2);
+                Console.WriteLine("total work : " + Totalwork);
+            }
+            else
+            {
+
+                Console.WriteLine("total work : " + Totalwork);
+            }
+        }
+    }
+}
+
