e36981d6f848879fa5f4d903ff59f7d0ad22f960
sumitradas94 <sumitradas94@gmail.com>
2023-01-04 12:24:28 +0530

    Switchcase empwage

---
+++ EmployeeSwitchCase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageOops
+{
+    public class EmployeeSwitchCase
+    {
+        public void Cal_Switchwage()
+        {
+            Console.WriteLine("Program for Calculating Employee Wage using Switch case!!");
+
+            int wagePerHour = 20;
+            int workingHours = 8;
+            int Totalwork = 0;
+            int Employeee = 1;
+            int atten = EmployeeAttendance.CheckAttendance(Employeee);
+
+            switch (atten)
+            {
+
+                case 1:
+                    //Console.WriteLine("\n employee is present");
+                    Totalwork = wagePerHour * workingHours;
+                    Console.WriteLine("total work : " + Totalwork);
+                    break;
+                case 2:
+
+                    Totalwork = wagePerHour * (workingHours / 2);
+                    Console.WriteLine("total work : " + Totalwork);
+                    break;
+
+                default:
+                    //Console.WriteLine("\n employee is absent");
+                    Console.WriteLine("total work : " + Totalwork);
+                    break;
+            }
+        }
+    }
+}
+
