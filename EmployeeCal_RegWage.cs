87950416a7d0f65886641451ce904a8347c3bd7a
sumitradas94 <sumitradas94@gmail.com>
2023-01-04 12:04:10 +0530

    Calculating the emp wage for regular

---
+++ EmployeeCal_RegWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageOops
+{
+    internal class EmployeeCal_RegWage
+    {
+    }
+}
