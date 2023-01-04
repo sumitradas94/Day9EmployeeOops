c6db170035afb2bcbfda7060ff0bb405f8a6368e
sumitradas94 <sumitradas94@gmail.com>
2023-01-04 12:17:14 +0530

    Update for UC3

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace EmployeeWageproblem
         public static void Main(string[] args)
         {
 
-            EmployeeCal_RegWage obj = new EmployeeCal_RegWage();
-            obj.Cal_Rwage();
-            
+            //EmployeeCal_RegWage obj = new EmployeeCal_RegWage();
+            //obj.Cal_Rwage();
+
+            EmployeeCal_PartTimeWage obj= new EmployeeCal_PartTimeWage();
+            obj.Cal_Pwage();
+
+
 
         }
     }
